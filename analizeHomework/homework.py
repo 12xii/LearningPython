2b70445de54af2d774cc2fb2f78a0babd318b9bb
XII <ur12xii@gmail.com>
2023-04-07 13:54:00 +0900

    [updated] 시각화

--- analizeHomework/homework.py
+++ analizeHomework/homework.py
@@ -1,12 +1,19 @@
 import csv
 
-path = '/Users/xii/LearningPython/202303_202303_연령별인구현황_월간.csv'
+path = '/Users/xii/LearningPython/analizeHomework/refer/202303_202303_연령별인구현황_월간 (3).csv'
 f = open(fr'{path}', encoding='cp949')
 data = csv.reader(f)
+
+newRow = []
 
 city = input('사시는 동네 이름을 작성해주세요 : ')
 for row in data :
   if city in row[0]:
     for num in row[3:]:
-      num = int(num.replace(',', ''))
-      print(num)
+      newRow.append(int(num.replace(',', '')))
+
+print(newRow)
+
+import matplotlib.pyplot as plt
+
+plt.plot(newRow)
